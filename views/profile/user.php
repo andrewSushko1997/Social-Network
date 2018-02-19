7bde0acdc0765d1bf602ea7dd01b0fcee7a4582e
sushko <sushko@scand.com>
2018-02-19 16:15:34 +0300

    Add friends and profile photos section

--- views/profile/user.php
+++ views/profile/user.php
@@ -1,16 +1,63 @@
 <div class="col-md-2">
     <div class="list-group">
         <button type="button" class="list-group-item list-group-item-action btn">Cras justo</button>
-        <button type="button" class="list-group-item list-group-item-action btn">Dapibus ac </button>
+        <button type="button" class="list-group-item list-group-item-action btn">Dapibus ac</button>
         <button type="button" class="list-group-item list-group-item-action btn">Morbi leo risus</button>
         <button type="button" class="list-group-item list-group-item-action btn">Porta ac consectetur</button>
     </div>
 </div>
 <div class="col-md-3">
     <div class="card profile-image-container">
-        <img class="card-img-top profile-image" src="https://pp.userapi.com/c841624/v841624875/36ec4/3ZrJm7iKSLI.jpg" alt="Card image cap">
+        <img class="card-img-top profile-image" src="https://pp.userapi.com/c841624/v841624875/36ec4/3ZrJm7iKSLI.jpg"
+             alt="Card image cap">
         <div class="card-body">
             <a href="#" class="btn btn-primary btn-block">Edit Profile</a>
+        </div>
+    </div>
+    <div class="card profile-friends">
+        <h4 class="card-header">Friends</h4>
+        <hr>
+        <div class="card-body">
+            <div class="row text-center">
+                <div class="col-md-4">
+                    <div class="profile-friends-container">
+                        <img src="https://pp.userapi.com/c621700/v621700299/17383/KAFdzCMfbx0.jpg" alt="">
+                        <h6>Pavel</h6>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="profile-friends-container">
+                        <img src="https://pp.userapi.com/c621700/v621700299/17383/KAFdzCMfbx0.jpg" alt="">
+                        <h6>Pavel</h6>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="profile-friends-container">
+                        <img src="https://pp.userapi.com/c621700/v621700299/17383/KAFdzCMfbx0.jpg" alt="">
+                        <h6>Pavel</h6>
+                    </div>
+                </div>
+            </div>
+            <div class="row text-center">
+                <div class="col-md-4">
+                    <div class="profile-friends-container">
+                        <img src="https://pp.userapi.com/c621700/v621700299/17383/KAFdzCMfbx0.jpg" alt="">
+                        <h6>Pavel</h6>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="profile-friends-container">
+                        <img src="https://pp.userapi.com/c621700/v621700299/17383/KAFdzCMfbx0.jpg" alt="">
+                        <h6>Pavel</h6>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="profile-friends-container">
+                        <img src="https://pp.userapi.com/c621700/v621700299/17383/KAFdzCMfbx0.jpg" alt="">
+                        <h6>Pavel</h6>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
@@ -21,12 +68,12 @@
         <hr>
         <div class="card-body">
             <div class="row">
-                <div class="col-md-6">
+                <div class="col-md-3">
                     <div>Birthday:</div>
                     <div>Current city:</div>
                     <div>Company:</div>
                 </div>
-                <div class="col-md-6">
+                <div class="col-md-9">
                     <div>July 15</div>
                     <div>Minsk</div>
                     <div>Scand</div>
@@ -34,5 +81,31 @@
             </div>
         </div>
         <hr>
+    </div>
+    <div class="card profile-photos">
+        <div class="card-header">
+            <h4 class="photos-header">My photos</h4>
+        </div>
+        <hr>
+        <div class="card-body  text-center">
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="photo-container"
+                         style="background-image: url(https://pp.userapi.com/c841624/v841624875/36ec4/3ZrJm7iKSLI.jpg);"></div>
+                </div>
+                <div class="col-md-3">
+                    <div class="photo-container"
+                         style="background-image: url(https://pp.userapi.com/c841624/v841624875/36ec4/3ZrJm7iKSLI.jpg);"></div>
+                </div>
+                <div class="col-md-3">
+                    <div class="photo-container"
+                         style="background-image: url(https://pp.userapi.com/c841624/v841624875/36ec4/3ZrJm7iKSLI.jpg);"></div>
+                </div>
+                <div class="col-md-3">
+                    <div class="photo-container"
+                         style="background-image: url(https://pp.userapi.com/c841624/v841624875/36ec4/3ZrJm7iKSLI.jpg);"></div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
